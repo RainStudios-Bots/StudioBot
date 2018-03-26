a8798f1015703f1f31c2d6053076413582d0ed4e
Spiderlogical <thehelpfulspider@gmail.com>
2018-03-25 21:28:47 -0700

    Adding some events

--- app.js
+++ app.js
@@ -7,11 +7,38 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('messageDelete', msg => {
+  if (msg.author.bot) return;
+  if (!msg) return msg.reply("Something went wrong?")
+  let modlog = msg.guild.channels.find("name", "logs");
+  if (!modlog) return msg.reply("Something went wrong! (Could not find modlog)")
+  let deleteEmbed = new Discord.RichEmbed()
+  .addField("Message Author: ", msg.author.tag)
+  .addField("Message Content: ", msg.content)
+  .addField("In channel: ", msg.channel)
+  .setThumbnail(msg.author.displayAvatarURL)
+  .setTimestamp();
+  modlog.send(deleteEmbed);
+});
 
+client.on('messageUpdate', (oldMessage, newMessage) => {
+  if (oldMessage.author.bot) return;
+  if (!oldMessage || !newMessage) return oldMessage.reply("Something went wrong?")
+  let modlog = oldMessage.guild.channels.find("name", "logs");
+  if (!modlog) return oldMessage.reply("Something went wrong! (Could not find modlog)")
+  let editEmbed = new Discord.RichEmbed()
+  .setDescription("Before Edit: " + oldMessage.content + ", After Edit: " + newMessage.content)
+  .addField("Message Author: ", newMessage.author.tag)
+  .addField("In channel: ", newMessage.channel)
+  .setThumbnail(newMessage.author.displayAvatarURL)
+  .setTimestamp();
+  modlog.send(editEmbed);
+});
+
+client.on('message', msg => {
  if (msg.author.bot) return;
  if(msg.content.indexOf(config.prefix) !== 0) return;
-
+if (command.channel.id != "427646753215610881") return msg.reply("The bot is in development, try again later!")
  const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
  const command = args.shift().toLowerCase();
 
