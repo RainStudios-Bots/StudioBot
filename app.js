e99c4aca1785a1e21b356af84fa2ebd46998d390
Spiderlogical <thehelpfulspider@gmail.com>
2018-03-26 19:54:53 -0700

    More Updates
    Adding presence, working on stuff and embeds

--- app.js
+++ app.js
@@ -4,15 +4,49 @@ const fs = require('fs');
 const ddif = require('return-deep-diff')
 const config = require('./config.json');
 const prefix = config.prefix;
+
+/* function changeGame() {
+
+  let gameNum = 0;
+
+  //30000
+  //900000
+    setInterval(function gameNumAdd() {
+      gameNum++;
+      return gameNum;
+    }, 30000)
+    console.log(gameNum);
+    if (gameNum > 3) {
+    gameNum = 0;
+    }
+    let gameType = 'PLAYING';
+    if (gameNum === 3) {
+      gameType = 'WATCHING';
+    } else gameType = 'PLAYING';
+    let games = ['Singin\' in the rain.', `With ${client.users.size} users!`, `In ${client.guilds.size} guilds`, 'Memes'];
+    let gamePres = games[gameNum];
+
+    return client.user.setPresence({
+        game: {
+          name: '~help | ' + gamePres,
+          type: gameType,
+        },
+        status: 'online'
+      })
+      .catch(console.error);
+  } */
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({
-      game: {
-        name: '~help | Singin\' in the rain.'
-      },
-      status: 'online'
-    })
-    .catch(console.error);
+    game: {
+      name: '~help | Singin\' in the rain!'
+    },
+    status: 'online'
+  })
+  .catch(console.error);
+}
+  // changeGame();
 });
 
 client.on('message', msg => {
@@ -38,6 +72,8 @@ client.on('messageDelete', msg => {
   let modlog = msg.guild.channels.find("name", "logs");
   if (!modlog) return msg.reply("Something went wrong! (Could not find modlog)")
   let deleteEmbed = new Discord.RichEmbed()
+  .setTitle("Message Deleted")
+  .setAuthor(guild.name, guild.iconURL)
     .addField("Message Author: ", msg.author.tag)
     .addField("Message Content: ", msg.content)
     .addField("In channel: ", msg.channel)
@@ -53,7 +89,9 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
   let modlog = oldMessage.guild.channels.find("name", "logs");
   if (!modlog) return oldMessage.reply("Something went wrong! (Could not find modlog)")
   let editEmbed = new Discord.RichEmbed()
-    .setDescription("Before Edit: " + oldMessage.content + ", After Edit: " + newMessage.content)
+  .setTitle("Message Edit")
+  .setAuthor(guild.name, guild.iconURL)
+    .setDescription("**Before Edit**: " + "\n" + oldMessage.content + ", **After Edit**: " + "\n" + newMessage.content)
     .addField("Message Author: ", newMessage.author.tag)
     .addField("In channel: ", newMessage.channel)
     .setThumbnail(newMessage.author.displayAvatarURL)
@@ -66,6 +104,8 @@ client.on('roleCreate', Role => {
   let modlog = Role.guild.channels.find("name", "logs");
   if (!modlog) return Role.guild.defaultChannel.send("Something went wrong! (Could not find modlog)")
   let newRoleEmbed = new Discord.RichEmbed()
+  .setTitle("Role Created")
+  .setAuthor(guild.name, guild.iconURL)
     .addField("Role: ", "`" + Role.name + "`" + " has been created")
     .setThumbnail(Role.guild.iconURL)
     .setTimestamp();
@@ -77,6 +117,7 @@ client.on('roleDelete', Role => {
   if (!modlog) return Role.guild.defaultChannel.send("Something went wrong! (Could not find modlog)")
   let deleteRoleEmbed = new Discord.RichEmbed()
     .setTitle("Role Removed")
+    .setAuthor(guild.name, guild.iconURL)
     .addField("Role: ", "`" + Role.name + "`" + " has been deleted")
     .setThumbnail(Role.guild.iconURL)
     .setTimestamp();
@@ -104,11 +145,25 @@ client.on('guildBanAdd', (guild, user) => {
   if (!modlog) return guild.defaultChannel.send("Something went wrong! (Could not find modlog)");
   let banAdd = new Discord.RichEmbed()
     .setTitle("Ban has been added")
+    .setAuthor(guild.name, guild.iconURL)
     .setThumbnail(user.displayAvatarURL)
     .addField("Member Banned: ", user.tag `(${user.id})`)
     .addField("At: ", new Date())
     .setTimestamp();
-  modlog.send(banAdd)
-})
+  modlog.send(banAdd);
+});
+
+client.on('guildBanRemove', (guild, user) => {
+  let modlog = guild.channels.find("name", "logs");
+  if (!modlog) return guild.defaultChannel.send("Something went wrong! (Could not find modlog)");
+  let banRemove = new Discord.RichEmbed()
+    .setTitle("Ban has been removed")
+    .setAuthor(guild.name, guild.iconURL)
+    .setThumbnail(user.displayAvatarURL)
+    .addField("Member Banned: ", user.tag `(${user.id})`)
+    .addField("At: ", new Date())
+    .setTimestamp();
+  modlog.send(banRemove);
+});
 
 client.login(config.token);
